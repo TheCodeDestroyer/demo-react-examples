c91c5469b058694df6df05d8db825288f2618b66
Nace Logar <the.code.destroyer@gmail.com>
2017-03-30 15:31:58 +0200

    Added subreddit api example code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/App'
-import './index.css';
+import 'babel-polyfill';
+
+import React from 'react';
+import {render} from 'react-dom';
+import Root from './containers/Root';
 
 render(
-  <App />,
-  document.getElementById('root')
+    <Root />,
+    document.getElementById('root')
 );
