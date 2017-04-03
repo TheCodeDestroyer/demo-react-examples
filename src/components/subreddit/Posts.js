7534d81fa24fbbcbc91aa7a3f3adab340dfe6a73
Nace Logar <the.code.destroyer@gmail.com>
2017-04-03 08:40:19 +0200

    Added link to subreddit posts

--- src/components/subreddit/Posts.js
+++ src/components/subreddit/Posts.js
@@ -2,10 +2,11 @@ import React, {PropTypes, Component} from 'react';
 
 export default class Posts extends Component {
     render() {
+        const redditBaseUrl = 'https://www.reddit.com';
         return (
             <ul>
                 {this.props.posts.map((post, i) =>
-                    <li key={i}>{post.title}</li>
+                    <li key={i}>{post.title} <a target="_blank" href={redditBaseUrl + post.permalink}>Link</a></li>
                 )}
             </ul>
         );
